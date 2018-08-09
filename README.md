4547e772bdfe5fb4d8bb85b1e0ecbc44a44eed5c
Miodrag Radosavljević <cinnamontress@gmail.com>
2018-08-09 22:46:33 +0200

    routes explanation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Petomatic
+
+#### Routes
+
+Route | What it does
+------ | -----------
+`api/validate` | validates login data
+`api/logout` | closes session
+`api/visittypes` | returns visit types table
+`api/visit/add` | adds a new visit to the db' table _visits_
+`api/visits` | return todays visits
+`api/visit/{id}/edit` | edits a particular visit
+`api/owners` | returns a list of customers/pet owners
+`api/owner/{id}/pets` | returns all customers pets
--- routes.php
+++ routes.php
@@ -1,16 +1,10 @@
 <?php
 
 // authentication
-$router->get('api/login', 'Authenticate@login');
 $router->get('api/logout', 'Authenticate@logout');
 $router->post('api/validate', 'Authenticate@validate');
 
 // browse
-$router->get('api/create', 'PagesController@create', true);
-$router->get('api/edit/{id}', 'PagesController@edit', true);
-$router->get('api/update/{id}', 'PagesController@update', true);
-$router->get('api/delete/{id}', 'PagesController@destroy', true);
-
 $router->get('api/owners', 'OwnersController@getowners');
 $router->get('api/owner/{id}/pets', 'OwnersController@getpetsbyowner');
 
